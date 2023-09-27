6dca4dd7dea238a7d4654fa569a3bdacde51f579
IJE2704 <mh1669101@gmail.com>
2023-09-27 21:05:59 +0600

    regiser page => get value from user input

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Register = () => {
+    // declare 2 states for store data
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState(null);
+    // here is function when change in the email input field then the value will get here =onChange
+    const handleEmailChange = (event) => {
+        // console.log(event.target.value);
+    }
+    // here is the function when after type the password your mouse will go out from the password input field then it will get the password value
+    const handlePasswordBlur = (event) =>{
+        // console.log(event.target.value);
+    }
+    // this function for when click the register button then it will get the user input value
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        // console.log(event.target.email.value);
+        // console.log(event.target.password.value);
+        const getEmail= event.target.email.value;
+        const getPassword = event.target.password.value;
+        setEmail(getEmail);
+        setPassword(getPassword);
+        console.log(email)
+        console.log(password);
+    }
+
     return (
         <div>
             <h1 className='text-3xl font-bold my-5'>register</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
 
-             <input className='border-2 mb-2' type="email" placeholder='Email' /><br />
-             <input className='border-2 mb-2' type="password" placeholder='Password' /> <br />
-             <button className='border-2 mb-2'>Register</button>
+                <input onChange={handleEmailChange} className='border-2 mb-2' id='email' type="email" placeholder='Email' /><br />
+                <input onBlur={handlePasswordBlur} className='border-2 mb-2' type="password" id='password' placeholder='Password' /> <br />
+                <button className='border-2 mb-2' type='submit'>Register</button>
             </form>
         </div>
     );
 };
 
-export default Register;<h1>register</h1>
+export default Register; <h1>register</h1>
