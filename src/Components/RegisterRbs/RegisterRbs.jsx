7d7b05ff990851e1ab7b17e1dcdef3f3555e6c54
IJE2704 <mh1669101@gmail.com>
2023-09-27 23:28:24 +0600

    error message , success message ,password validation

--- src/Components/RegisterRbs/RegisterRbs.jsx
+++ src/Components/RegisterRbs/RegisterRbs.jsx
@@ -1,24 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import{createUserWithEmailAndPassword, getAuth} from 'firebase/auth'
 import app from '../../Firebase/firebase.config';
-import { Result } from 'postcss';
+
 
 const RegisterRbs = () => {
+    const [errorMessage,setErrorMessage] = useState('');
+    const [successMessage,setSuccessMessage] = useState('');
     const handleRegisterRbs = event =>{
+        
         event.preventDefault();
         const email = event.target.email.value;
         const password = event.target.password.value;
+        // errorMessage='';
         console.log(email,password);
+        setSuccessMessage('');
+        setErrorMessage('');
+        if(!/(?=.*[a-z])/.test(password))
+        {
+           setErrorMessage('Add at least one lowercase latter!')
+           return
+        }
+        else if(!/(?=.*[A-Z])/.test(password))
+        {
+           setErrorMessage('Add at least one uppercase latter!')
+           return
+        }
+        else if(!/(?=.*\d)/.test(password))
+        {
+           setErrorMessage('Add at least one digit!')
+           return
+        }
+        else if(!/.{8,}/.test(password))
+        {
+           setErrorMessage('Password length at least 8!')
+           return
+        }
         const auth = getAuth(app);
         createUserWithEmailAndPassword(auth,email,password)
         .then(result =>{
             const loggedUser = result.user;
+            event.target.reset();
             console.log(loggedUser);
+            // errorMessage=null;
+            
+            setSuccessMessage('User added successfully.')
         })
         .catch(error =>{
             console.error(error);
+            setErrorMessage(error.message);
+           
         })
         
     }
@@ -27,19 +59,25 @@ const RegisterRbs = () => {
             <Form onSubmit={handleRegisterRbs}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" name="email" placeholder="Enter email" />
+                    <Form.Control type="email" required  name="email" placeholder="Enter email" />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" name="password" placeholder="Password" />
+                    <Form.Control type="password" required name="password" placeholder="Password" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Accept Terms and conditions" />
+                    <Form.Check type="checkbox" required label="Accept Terms and conditions" />
                 </Form.Group>
                 <Button  className='border-3 text-black' type="submit">
                     Submit
                 </Button>
+                {
+                    <div>
+                        <p className='text-danger'>{errorMessage}</p>
+                    <p className='text-success'>{successMessage}</p>
+                    </div>
+                }
             </Form>
         </div>
     );
